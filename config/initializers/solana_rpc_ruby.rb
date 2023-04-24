96dd59bffb41c07218cb70375efc3883aecf53a7
Richard Fisher <richardfsr@gmail.com>
2023-04-24 14:38:18 +0100

    use hellomoon rpc

--- config/initializers/solana_rpc_ruby.rb
+++ config/initializers/solana_rpc_ruby.rb
@@ -6,8 +6,10 @@ SolanaRpcRuby.config do |c|
   # These are options that you can set before using gem:
   #
   # You can use this setting or pass cluster directly, check the docs.
-  c.cluster = ENV['STORE_RPC']
+  c.cluster = ENV['RPC']
 
   # This one is mandatory.
   c.json_rpc_version = '2.0'
+
+  c.bearer_token = ENV['HELLOMOON_TOKEN']
 end
