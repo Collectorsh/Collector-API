c147383c5e4302ab903b503a8bd8649625440e7d
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2023-06-26 15:46:21 -0700

    allow all galleries

--- app/controllers/visibility_controller.rb
+++ app/controllers/visibility_controller.rb
@@ -3,33 +3,6 @@
 gem 'activerecord-import'
 
 class VisibilityController < ApplicationController
-  # def old_set_visibility_and_order
-  #   user = User.find_by_api_key(params[:api_key])
-  #   return render json: { status: 'error', msg: 'Api key not valid' } unless user
-
-  #   user.update_attribute(:columns, params[:columns]) if params[:columns]
-
-  #   mints = MintVisibility.where(user_id: user.id).pluck(:mint_address)
-  #   mints -= params[:tokens].collect { |t| t['mint'] }.flatten
-  #   MintVisibility.where(user_id: user.id, mint_address: mints).delete_all
-
-  #   MintVisibility.transaction do
-  #     params[:tokens].each do |token|
-  #       visibility = MintVisibility.find_or_initialize_by(user_id: user.id, mint_address: token['mint'])
-  #       visibility.assign_attributes(
-  #         visible: token['visible'],
-  #         order_id: token['order_id'],
-  #         accept_offers: token['accept_offers'],
-  #         image: token['uri'],
-  #         span: token['span'],
-  #       )
-  #       visibility.save!
-  #     end
-  #   end
-
-  #   render json: { status: 'success' }
-  # end
-
   def set_visibility_and_order
     begin
       user = User.find_by_api_key(params[:api_key])
