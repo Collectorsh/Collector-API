8895fe8fee722d2dd87a12bf947837760a65b586
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2023-07-19 13:41:41 -0700

    add index to optimized images on mint_address

---
+++ db/migrate/20230912034104_add_index_to_optimized_images_for_mint_address.rb
@@ -0,0 +1,10 @@
+Copy code
+class AddIndexToOptimizedImagesForMintAddress < ActiveRecord::Migration[6.1]
+  def up
+    execute 'CREATE INDEX index_optimized_images_on_mint_address ON optimized_images (mint_address)'
+  end
+
+  def down
+    execute 'DROP INDEX index_optimized_images_on_mint_address'
+  end
+end
