7b309db67e69e5de117f03342503a51d46d3f013
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2023-10-10 16:12:52 -0700

    individual dockers

--- env_to_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exclude some sensitive or system-specific environment variables
-# Adjust the list of exclusions as needed
-exclude="^(PATH|PS1|HOME|HOSTNAME|TERM|SHELL|USER|LANG|LS_COLORS|PWD|_)=.*"
-
-# Capture all environment variables
-env | grep -vE "$exclude" > .env
