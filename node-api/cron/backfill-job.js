d1f4b8f59453f734d7079baa607161e2780164d4
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2023-11-22 10:57:53 -0800

    backfill cron only in prod

--- node-api/cron/backfill-job.js
+++ node-api/cron/backfill-job.js
@@ -2,7 +2,11 @@ import cron from "node-cron"
 import { backfill } from "../src/scripts/backfill.js";
 
 export const runBackfillJob = () => {
-  console.log("init cron")
+  if (process.env.STAGE !== "production") {
+    console.log("Not in production, not initializing Backfill Cron Job")
+    return
+  }
+  console.log("Initialize Backfill Cron Job")
   cron.schedule('1 6 * * *', async function() {
     console.log("---------------------")
     console.log("Running Backfill Cron Job");
