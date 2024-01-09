1a9fa2c19965f139b95462418e8ce5e9af0eb963
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2024-01-09 12:49:16 -0800

    cable error handling 2

--- ruby-api/app/channels/application_cable/channel.rb
+++ ruby-api/app/channels/application_cable/channel.rb
@@ -1,4 +1,7 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    rescue_from RuntimeError do |e|
+      Rails.logger.error("RuntimeError in Cable channel: #{e.message} - Backtrace: #{e.backtrace.join("\n")}")
+    end
   end
 end
