f12cc8b7cdc97c1ac39ff9fa32c54cac159d1def
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2024-01-09 17:48:32 -0800

    fix error log in cable

--- ruby-api/app/channels/application_cable/connection.rb
+++ ruby-api/app/channels/application_cable/connection.rb
@@ -1,18 +1,12 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     rescue_from RuntimeError do |e|
-      log_error("RuntimeError", e)
+      Rails.logger.error("Runtime in Cable connection: #{e.message} - Backtrace: #{e.backtrace.join("\n")}")
     end
   end
 
   
   def report_error(e)
     Rails.logger.error("Action Cable Error: #{e.message} - Backtrace: #{e.backtrace.join("$/")}")
-  end
-
-  private
-
-  def log_error(error_type, e)
-    Rails.logger.error("#{error_type} in Cable connection: #{e.message} - Backtrace: #{e.backtrace.join("\n")}")
   end
 end
