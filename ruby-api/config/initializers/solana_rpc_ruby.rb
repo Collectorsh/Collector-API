9cf419bf20e5433e3761c21ee96dd740c2e39cd9
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2024-01-29 20:42:48 -0800

    record sales update

--- ruby-api/config/initializers/solana_rpc_ruby.rb
+++ ruby-api/config/initializers/solana_rpc_ruby.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-require_relative 'solana_rpc_ruby'
+# require_relative 'solana_rpc_ruby'
 
-SolanaRpcRuby.config do |c|
-  # These are options that you can set before using gem:
-  #
-  # You can use this setting or pass cluster directly, check the docs.
-  c.cluster = ENV['RPC']
+# SolanaRpcRuby.config do |c|
+#   # These are options that you can set before using gem:
+#   #
+#   # You can use this setting or pass cluster directly, check the docs.
+#   c.cluster = ENV['RPC']
 
-  # This one is mandatory.
-  c.json_rpc_version = '2.0'
+#   # This one is mandatory.
+#   c.json_rpc_version = '2.0'
 
-  c.bearer_token = ENV['HELLOMOON_TOKEN']
-end
+#   c.bearer_token = ENV['HELLOMOON_TOKEN']
+# end
