0c7f0c9c7dc047d0f44731bc7698aa04974f9dbf
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2024-03-21 12:23:56 -0700

    dup

--- ruby-api/db/migrate/20290250474619_create_curations.rb
+++ ruby-api/db/migrate/20290250474619_create_curations.rb
@@ -19,7 +19,9 @@ class CreateCurations < ActiveRecord::Migration[6.1]
       t.timestamps
     end
 
-    add_index :curations, :name, unique: true
+    # add_index :curations, :name, unique: true
+    add_index :curations, [:curator_id, :name], unique: true
+
     add_index :curations, :approved_artist_ids, using: 'gin' 
     add_foreign_key :curations, :users, column: :curator_id
   end
